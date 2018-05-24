e21cab0fb9ee8f2dfd0d6a60393e12e85bee029c
Helerr <alexandruneculai04@gmail.com>
2018-05-24 15:57:34 +0300

    add RSS feed tests/add menu hidden test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,29 +31,48 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('has a URL defined and it is not empty', function(){
+           allFeeds.forEach(function(element){
+             expect(element.url).toBeDefined();
+             expect(element.url).not.toBe(0);
+           });
+           }
+         );
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has a name defined and it is not empty', function(){
+           allFeeds.forEach(function(element){
+             expect(element.name).toBeDefined();
+             expect(element.name).not.toBe(0);
+           });
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe("The menu", function(){
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('has the menu hidden by default', function(){
+           expect($("body").hasClass("menu-hidden")).toBe(true);
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it("displays the menu when the menu icon is clicked", function(){
 
+          })
+        });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
